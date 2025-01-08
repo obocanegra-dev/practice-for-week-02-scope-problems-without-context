8bf7b3dad89e607ee1853bae64848cbba5f40b72
obocanegra-dev <oscarbocanegraush@gmail.com>
2025-01-08 04:05:38 +0000

    problem 16 done!

--- problems/16-coupon.js
+++ problems/16-coupon.js
@@ -15,7 +15,7 @@ console.log(twentyPercent([10, 20, 30])); // [ 8, 16, 24 ]
 
 ***********************************************************************/
 function coupon(discount) {
-  // Your code here
+    return prices => prices.map(price => price - price * discount);
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
