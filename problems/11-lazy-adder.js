eb6638450f11761beec1cb3f8d8dfe9db0e7d166
obocanegra-dev <oscarbocanegraush@gmail.com>
2025-01-08 02:07:13 +0000

    problem 11 done!

--- problems/11-lazy-adder.js
+++ problems/11-lazy-adder.js
@@ -23,7 +23,7 @@ AFTER YOU ARE FINISHED WITH THIS PROBLEM, ASK FOR A CODE REVIEW
 ***********************************************************************/
 
 function lazyAdder(firstNum) {
-  // Your code here
+    return secondNum => thirdNum => firstNum + secondNum + thirdNum;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
