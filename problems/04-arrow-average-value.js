9c674e10fd9a8c17f07e5534e8de688350ace940
obocanegra-dev <oscarbocanegraush@gmail.com>
2025-01-08 01:22:28 +0000

    problem 04 done!

--- problems/04-arrow-average-value.js
+++ problems/04-arrow-average-value.js
@@ -11,7 +11,13 @@ arrowAvgValue([100, 60, 64]); // => 74.66666666666667
 ***********************************************************************/
 
 const arrowAvgValue = array => {
-  // Your code here
+    let sum = 0;
+
+    array.forEach(num => {
+      sum += num;
+    });
+
+    return sum / array.length;
 };
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
